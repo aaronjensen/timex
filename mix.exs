a3e37725e73aab8c4c72ef5a67159b5f57b736dd
Aaron Jensen <aaronjensen@gmail.com>
2016-06-16 13:16:16 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Timex.Mixfile do
 
   def project do
     [ app: :timex,
-      version: "2.1.6",
+      version: "2.1.6+1",
       elixir: "~> 1.1",
       description: description,
       package: package,
